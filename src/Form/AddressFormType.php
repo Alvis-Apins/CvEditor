bb28ed0e8b42a6e1530aa04b29d9d9771f6cf46d
Alvis Apins <alvis.apins.it@gmail.com>
2022-10-03 14:06:43 +0300

    Update AddressFormType.php

--- src/Form/AddressFormType.php
+++ src/Form/AddressFormType.php
@@ -44,8 +44,8 @@ class AddressFormType extends AbstractType
             ->add('house_name', TextType::class, [
                 'attr' => [
                     'class' => 'h-10 shadow-lg block p-4 mb-8 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                    'placeholder' => 'optional'
                 ],
-                'placeholder' => 'optional',
                 'required' => false,
                 'label_attr' => [
                     'class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
